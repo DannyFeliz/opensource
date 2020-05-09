27f3b24179f12d9aaaf0b78a35ae5cc267e63cad
Danny Feliz <dannyfeliz08@gmail.com>
2020-05-09 19:31:27 -0400

    Add user variables

---
+++ config/user.js
@@ -0,0 +1,15 @@
+const locations = ["dominican", "santo domingo"];
+const usernamesToInclude = [
+  "psanders",
+  "claudiosanchez",
+  "enmanuel97",
+  "smartdev",
+  "davidtavarez",
+  "developersdo",
+  "sorribas",
+];
+
+module.exports = {
+  locations,
+  usernamesToInclude
+}
